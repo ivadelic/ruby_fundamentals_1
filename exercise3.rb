820c15ad39f6bde84f3c0ccf25820493b5dab228
Iva Delic <ms.iva.delic@gmail.com>
2015-03-31 17:43:51 -0400

    Updated All exercises

--- exercise3.rb
+++ exercise3.rb
@@ -1,6 +1,7 @@
+t = Time.new
 puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name}!"
 puts "How old are you or turning this year?"
-age = gets.chomp.to_i
-puts "You were born in #{2015-age}!"
+age = gets.chomp.to_i 
+puts "You were born in #{ t.year - age}!"
--- exercise4.rb
+++ exercise4.rb
@@ -1,11 +1,11 @@
 to_ten = (1..100)
 to_ten.each do |num|
 	if num.remainder(3)==0
-		puts "Bit"
+		puts "BIT"
 	elsif num.remainder(5)==0
-		puts "Maker"
+		puts "MAKER"
 	elsif num.remainder(15)==0
-		puts "BitMaker"
+		puts "BITMAKER"
 	else 
 		puts "#{num}"
 	end
