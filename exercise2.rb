35a4f35c166aea102f2c29367ade715c0f243c0b
Iva Delic <ms.iva.delic@gmail.com>
2015-03-31 14:37:00 -0400

    Finished assignment 2

---
+++ exercise2.rb
@@ -0,0 +1,13 @@
+# meal= 55
+# good_tip = meal * 0.20 
+# puts "A good tip for a #{meal} dollar meal would be #{good_tip} dollars. The total cost with tip would be #{meal + good_tip} dollars."
+
+# puts "3" + 4.to_s
+
+# answer = 45628*7839 
+# puts "#{answer} is your answer"
+
+x = (true && false) || (false && true) || !(false && false) 
+puts x
+#false || false || true
+#true 
