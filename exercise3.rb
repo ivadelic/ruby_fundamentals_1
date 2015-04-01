2f4a3ec8c14159f6ffaa8ee3225de71bfd66741d
Iva Delic <ms.iva.delic@gmail.com>
2015-03-31 23:12:05 -0400

    Added a few fun stuff

--- exercise3.rb
+++ exercise3.rb
@@ -2,6 +2,14 @@ t = Time.new
 puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name}!"
+system ("say Hello #{name}")
 puts "How old are you or turning this year?"
 age = gets.chomp.to_i 
 puts "You were born in #{ t.year - age}!"
+system ('say "OLD"')
+puts "You like to boogie?"
+answer = gets.chomp
+system ('say "boogie boogie boogie, i like to party"')
+puts "You want to boogie more like a true boogier?"
+answer2 = gets.chomp
+system ('say "boogie boogie boogie, boogie boogie boogie, hallo oskarz"')
